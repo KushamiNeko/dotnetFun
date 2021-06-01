934e71818e8fcc5dbbd4e53a601775aa0f7fcf64
Kushami Neko <missing10630@gmail.com>
2021-06-02 02:32:06 +0900

    use data storage as source of contents

--- BlazorAppHttps/Data/StepService.cs
+++ BlazorAppHttps/Data/StepService.cs
@@ -4,35 +4,48 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace BlazorAppHttps.Data
 {
     public class StepService
     {
 
-        public List<Step> Steps
+        // public List<Step> Steps
+        // {
+        //     get
+        //     {
+        //         return _steps;
+        //     }
+        // }
+
+        // private List<Step> _steps;
+
+        // public StepService()
+        // {
+        //     _steps = GetSteps();
+        // }
+
+        public async Task<List<Step>> GetStepsWeb()
         {
-            get
-            {
-                return _steps;
-            }
+
+            HttpClient client = new HttpClient();
+
+            Task<Stream> streamTask = client.GetStreamAsync("https://zutsukineko3339.blob.core.windows.net/$web/steps_web.json");
+            List<Step> steps = await JsonSerializer.DeserializeAsync<List<Step>>(await streamTask);
+
+            return steps;
         }
 
-        private List<Step> _steps;
-
-        public StepService()
+        public Task<List<Step>> GetStepsLocal()
         {
-            _steps = GetSteps();
-        }
 
-        private List<Step> GetSteps()
-        {
             string content = File.ReadAllText(Path.Join(Directory.GetCurrentDirectory(), @"/wwwroot/steps.json"));
 
             List<Step> steps =
                 JsonSerializer.Deserialize<List<Step>>(content);
 
-            return steps;
+            return Task.FromResult(steps);
         }
     }
 }
